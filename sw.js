fc28407d1f81492e63e1463b7ce81141a3d4fc49
Svetoslav <bogdanovsvetoslav@test.com>
2026-09-08 10:28:42 +0300

    Deploy v1.8.38

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "hab-hob-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v1`;
+const CACHE_NAME = `${CACHE_PREFIX}v2`;
 const appRoot = new URL("./", self.registration.scope);
 const shellFiles = [
   appRoot.href,
